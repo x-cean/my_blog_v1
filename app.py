353182e0a1ba68ac5f2b95ecd3b7308da7fa15b4
xiao <194778728+x-cean@users.noreply.github.com>
2025-05-22 01:11:44 +0200

    updating index route

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from flask import render_template
+import json
 
 app = Flask(__name__)
 
 
+def get_blog_posts():
+    try:
+        with open("blog_posts.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        with open("blog_posts.json", "w") as f:
+            json.dump([], f)
+            return []
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = get_blog_posts()
+    return render_template("index.html", posts=blog_posts)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
